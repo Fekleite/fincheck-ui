18a95b3cda26973c67d611bc27397a93cb70043e
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-01-15 14:43:19 -0300

    feat: create input component

---
+++ src/ui/components/Input.tsx
@@ -0,0 +1,28 @@
+import { InputHTMLAttributes } from "react";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+}
+
+export function Input({ placeholder, name, id, ...attrs }: InputProps) {
+  const inputId = id ?? name
+
+  return (
+    <div className="relative flex items-center">
+      <input 
+        {...attrs}
+        name={name}
+        id={inputId}
+        placeholder=" "
+        className="w-full bg-white rounded-lg border border-gray-500 focus:border-gray-800 px-3 pt-4 h-[52px] text-gray-800 text-sm outline-none peer placeholder-shown:pt-0"
+      />
+
+      <label 
+        htmlFor={inputId}
+        className="absolute left-3.5 top-2 pointer-events-none text-gray-700 text-xs peer-placeholder-shown:top-auto peer-placeholder-shown:text-sm transition-all"
+      >
+        {placeholder}
+      </label>
+    </div>
+  )
+}
