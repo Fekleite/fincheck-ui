77332e9bcc62e5e7eb67a1c0eb0855869bc5a753
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-01-15 14:46:21 -0300

    feat: create register page ui

--- src/ui/pages/Register/index.tsx
+++ src/ui/pages/Register/index.tsx
@@ -1,3 +1,33 @@
+import { Link } from "react-router-dom";
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
 export function Register() {
-  return <h1>Register page</h1>
+  return (
+    <div>
+      <header className="flex flex-col items-center gap-4">
+        <h1 className="text-2xl font-bold text-gray-900 -tracking-[1px]">Crie sua conta</h1>
+
+        <p className="space-x-2">
+          <span className="text-gray-700 -tracking-[0.5px]">Já possui uma conta?</span>
+
+          <Link to="/login" className="text-teal-900 font-medium -tracking-[0.5px]">Fazer Login</Link>
+        </p>
+      </header>
+
+      <form className="mt-14 flex flex-col gap-4">
+        <Input placeholder="Nome" type="text" name="name" />
+        <Input placeholder="E-mail" type="email" name="email" />
+        <Input placeholder="Senha" type="password" name="password" />
+
+        <Button 
+          type="submit"
+          className="mt-2"
+        >
+          Criar conta
+        </Button>
+      </form>
+    </div>
+  )
 }
