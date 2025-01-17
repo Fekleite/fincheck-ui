828e5f2c23436fe417049eb05b95d9760f46a7d5
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-01-16 22:39:37 -0300

    fix: allows button to merge classes

--- src/ui/components/Button.tsx
+++ src/ui/components/Button.tsx
@@ -1,12 +1,16 @@
 import { ButtonHTMLAttributes } from "react";
+import { cn } from "../../app/utils/cn";
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
-export function Button({ ...attrs }: ButtonProps) {
+export function Button({ className, ...attrs }: ButtonProps) {
   return (
     <button
       {...attrs}
-      className="h-12 rounded-2xl bg-teal-800 px-6 font-medium text-white transition-all hover:bg-teal-900 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-400"
+      className={cn(
+        "h-12 rounded-2xl bg-teal-800 px-6 font-medium text-white transition-all hover:bg-teal-900 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-400",
+        className,
+      )}
     />
   );
 }
