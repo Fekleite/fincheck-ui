8ba84c5478b4e4b7a56fd0ba3865ffdc85e8be69
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-01-20 21:00:55 -0300

    feat: add request interceptors to set authorization header

--- src/app/services/httpClient.ts
+++ src/app/services/httpClient.ts
@@ -1,5 +1,17 @@
 import axios from "axios";
 
+import { localStorageKeys } from "../config/localStorageKeys";
+
 export const httpClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+});
+
+httpClient.interceptors.request.use((req) => {
+  const accessToken = localStorage.getItem(localStorageKeys.ACCESS_TOKEN);
+
+  if (accessToken) {
+    req.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return req;
 });
