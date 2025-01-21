510b30c9e50726583c0489765ceeb736ae3416ee
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-01-21 20:45:12 -0300

    feat: add a spinner on page loader

--- src/ui/components/PageLoader.tsx
+++ src/ui/components/PageLoader.tsx
@@ -1,9 +1,12 @@
 import { Logo } from "./Logo";
+import { Spinner } from "./Spinner";
 
 export function PageLoader() {
   return (
-    <div className="fixed inset-0 flex h-full w-full items-center justify-center bg-teal-900">
-      <Logo className="h-10 animate-pulse text-white" />
+    <div className="fixed inset-0 flex h-full w-full flex-col items-center justify-center gap-2 bg-teal-900">
+      <Logo className="h-10 text-white" />
+
+      <Spinner className="h-5 w-5 fill-white text-teal-900" />
     </div>
   );
 }
