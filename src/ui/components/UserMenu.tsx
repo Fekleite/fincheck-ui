bd9beb114a747d0e0bb1283742aac5d57043cd73
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-01-22 15:42:38 -0300

    feat: create user menu component

---
+++ src/ui/components/UserMenu.tsx
@@ -0,0 +1,9 @@
+export function UserMenu() {
+  return (
+    <div className="flex h-12 w-12 items-center justify-center rounded-full border border-teal-100 bg-teal-50">
+      <span className="text-sm font-medium -tracking-[0.5] text-teal-900">
+        FL
+      </span>
+    </div>
+  );
+}
