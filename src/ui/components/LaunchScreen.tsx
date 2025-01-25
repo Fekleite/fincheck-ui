8f8a6de35f91b54ae420022fbb1ba7ed2f70fd4b
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-01-24 22:42:52 -0300

    fox: change launch screen z index

--- src/ui/components/LaunchScreen.tsx
+++ src/ui/components/LaunchScreen.tsx
@@ -10,7 +10,7 @@ interface LaunchScreenProps {
 export function LaunchScreen({ isLoading }: LaunchScreenProps) {
   return (
     <Transition show={isLoading}>
-      <div className="fixed inset-0 flex h-full w-full flex-col items-center justify-center gap-2 bg-teal-900 transition duration-300 ease-in data-[closed]:opacity-0">
+      <div className="fixed inset-0 z-50 flex h-full w-full flex-col items-center justify-center gap-2 bg-teal-900 transition duration-300 ease-in data-[closed]:opacity-0">
         <Logo className="h-10 text-white" />
 
         <Spinner className="h-5 w-5 fill-white text-teal-900" />
