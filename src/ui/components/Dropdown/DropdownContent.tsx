43b1d768cfb0f2bf158708b6b94e3901953aef30
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-01-28 22:27:17 -0300

    fix: define z-index

--- src/ui/components/Dropdown/DropdownContent.tsx
+++ src/ui/components/Dropdown/DropdownContent.tsx
@@ -12,7 +12,7 @@ export function DropdownContent({
       <DropdownMenu.Content
         {...attrs}
         className={cn(
-          "space-y-2 rounded-2xl border border-gray-100 bg-white p-2 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10)]",
+          "z-50 space-y-2 rounded-2xl border border-gray-100 bg-white p-2 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10)]",
           "data-[side=bottom]:animate-slide-up-and-fade data-[side=left]:animate-slide-right-and-fade data-[side=right]:animate-slide-left-and-fade data-[side=top]:animate-slide-down-and-fade",
           className,
         )}
