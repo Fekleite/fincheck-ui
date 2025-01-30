c6698597e19403c0ca9ebb768e1a7f0f918a8789
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-01-29 23:41:35 -0300

    feat: controle filter modal state

--- src/ui/pages/Dashboard/components/Transactions/useTransactionsController.ts
+++ src/ui/pages/Dashboard/components/Transactions/useTransactionsController.ts
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 export function useTransactionsController() {
+  const [isFilterModalOpen, setIsFiltersModalOpen] = useState(false);
+
+  function handleOpenFiltersModal() {
+    setIsFiltersModalOpen(true);
+  }
+
+  function handleCloseFiltersModal() {
+    setIsFiltersModalOpen(false);
+  }
+
   return {
     isInitialLoading: false,
     isLoading: false,
     transactions: [],
+    isFilterModalOpen,
+    handleOpenFiltersModal,
+    handleCloseFiltersModal,
   };
 }
