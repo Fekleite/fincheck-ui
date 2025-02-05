d79243ec5be68133b2feededf03848d10308fcd1
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-02-04 21:30:29 -0300

    feat: create button variants

--- src/ui/components/Button.tsx
+++ src/ui/components/Button.tsx
@@ -4,6 +4,7 @@ import { Spinner } from "./Spinner";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   isLoading?: boolean;
+  variant?: "danger" | "ghost";
 }
 
 export function Button({
@@ -11,6 +12,7 @@ export function Button({
   isLoading,
   disabled,
   children,
+  variant,
   ...attrs
 }: ButtonProps) {
   return (
@@ -19,6 +21,9 @@ export function Button({
       disabled={disabled || isLoading}
       className={cn(
         "flex h-12 items-center justify-center rounded-2xl bg-teal-900 px-6 font-medium text-white transition-all hover:bg-teal-800 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:text-gray-400",
+        variant === "danger" && "bg-red-900 hover:bg-red-800",
+        variant === "ghost" &&
+          "border border-gray-800 bg-transparent text-gray-800 hover:bg-gray-100",
         className,
       )}
     >
