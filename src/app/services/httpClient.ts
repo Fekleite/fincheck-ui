ad994c6a08e4bbf19a263ef065aaea637bc723a1
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-02-06 21:43:45 -0300

    refactor: add request delay

--- src/app/services/httpClient.ts
+++ src/app/services/httpClient.ts
@@ -1,17 +1,20 @@
 import axios from "axios";
 
 import { localStorageKeys } from "../config/localStorageKeys";
+import { sleep } from "../utils/sleep";
 
 export const httpClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
-httpClient.interceptors.request.use((req) => {
+httpClient.interceptors.request.use(async (req) => {
   const accessToken = localStorage.getItem(localStorageKeys.ACCESS_TOKEN);
 
   if (accessToken) {
     req.headers.Authorization = `Bearer ${accessToken}`;
   }
+
+  await sleep(1000);
 
   return req;
 });
